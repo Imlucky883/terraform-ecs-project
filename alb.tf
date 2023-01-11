85e67405dd2809009c280e3a3d5657a8b85d2d97
Laxman Patel <88871793+Imlucky883@users.noreply.github.com>
2023-01-11 14:14:03 +0530

    Updating ALB

---
+++ alb.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "my-alb" {
+  name               = "Gawin's-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.my_subnet.id
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+  name        = "alb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    matcher             = "200"
+    timeout             = 3
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.my_alb.id
+  port              = var.app_port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
-resource "aws_vpc" "my-vpc" {
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
 	Name = "Gawin-vpc"
   }
 }
 
-resource "aws_subnet" "my-subnet" {
-  vpc_id     = aws_vpc.my-vpc.id
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = variable.az_count
   tags = {
@@ -14,30 +14,29 @@ resource "aws_subnet" "my-subnet" {
   }
 }
 
-resource "aws_internet_gateway" "my-igw" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 
   tags = {
     Name = "Gawin's-igw"
   }
 }
 
-resource "aws_route_table" "my-rt" {
-  vpc_id = aws_vpc.my-vpc.id
+resource "aws_route_table" "my_rt" {
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
-    subnet_id  = aws_subnet.my-subnet.id
+    subnet_id  = aws_subnet.my_subnet.id
     cidr_block = "10.0.1.0/24"
-    
   }
   tags = {
     Name = "Gawin'-rt"
   }
 }
 
-resource "aws_route_table_association" "my-association" {
-  subnet_id      = aws_subnet.my-subnet.id
-  route_table_id = aws_route_table.my-rt.id
+resource "aws_route_table_association" "my_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
 }
 
 
